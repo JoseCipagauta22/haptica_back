48b85b011c8aed58c6c17752ab73074ec88b0144
Jose Cipagauta <jose.cipagauta52@gmail.com>
2024-08-09 11:08:04 -0500

    actualizar docker compose yml y actualizar servicio de categories

--- src/categories/services/categories.service.ts
+++ src/categories/services/categories.service.ts
@@ -8,40 +8,40 @@ import { CreateCategoryDto } from '../dtos/categories.dto';
 @Injectable()
 export class CategoriesService {
 
-    miTest: string = '';
-    categories: Category[] = [];
-    constructor(@InjectRepository(Category) private categoryRepo: Repository<Category>){}
+  miTest: string = '';
+  categories: Category[] = [];
+  constructor(@InjectRepository(Category) private categoryRepo: Repository<Category>){}
 
-    async findAll(){
-        this.categories = await this.categoryRepo.find();
-        this.categories.forEach(element => {
-            element.image = '../../../../assets/' + element.image
-        });
-        this.categories.sort(function (a, b) {
-            if (a.id > b.id) {
-              return 1;
-            }
-            if (a.id < b.id) {
-              return -1;
-            }
-            // a must be equal to b
-            return 0;
-        });
-        return this.categories;
-        // return this.categoryRepo.find();
-    }
+  async findAll(){
+      this.categories = await this.categoryRepo.find();
+      this.categories.forEach(element => {
+          element.image = '../../../../assets/' + element.image
+      });
+      this.categories.sort(function (a, b) {
+          if (a.id > b.id) {
+            return 1;
+          }
+          if (a.id < b.id) {
+            return -1;
+          }
+          // a must be equal to b
+          return 0;
+      });
+      return this.categories;
+      // return this.categoryRepo.find();
+  }
 
-    async findOne(id: number) {
-        const category = await this.categoryRepo.findOne({relations: ['items'], where: {id}});
-        if (!category) {
-          throw new NotFoundException(`Product #${id} not found`);
-        }
-        return category.items;
-    }
+  async findOne(id: number) {
+      const category = await this.categoryRepo.findOne({relations: ['items'], where: {id}});
+      if (!category) {
+        throw new NotFoundException(`Product #${id} not found`);
+      }
+      return category;
+  }
 
-    createCategory(data: CreateCategoryDto){
-        const newCategory = this.categoryRepo.create(data)
-        return this.categoryRepo.save(newCategory);
-    }
+  createCategory(data: CreateCategoryDto){
+      const newCategory = this.categoryRepo.create(data);
+      return this.categoryRepo.save(newCategory);
+  }
     
 }
