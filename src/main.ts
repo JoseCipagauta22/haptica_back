34393cf5877dd45f0f079ae375e8770954942d4d
Jose Cipagauta <jose.cipagauta52@gmail.com>
2024-10-28 08:26:00 -0500

    ssl conf 1.7

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,27 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as https from 'https';
-// import path from 'path';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const httpsOptions = {
-    key: fs.readFileSync('./../haptica-back/src/clave_privada.key'), 
-    cert: fs.readFileSync('./../haptica-back/src/certificado.crt'),
-  };
+  // Redirección de HTTP a HTTPS
+  app.use((req, res, next) => {
+    if (req.protocol === 'http') {
+      return res.redirect('https://${req.headers.host}${req.url}');
+    }
+    next();
+  });
+
+  // Configuración de proxy para Render
+  app.set('trust proxy', 1);
 
   app.enableCors({
-      // origin: '*', // Change this to your frontend origin
-      // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      // credentials: true,
-    }
-  );
+    origin: 'https://tu-frontend.com', // Cambia esto por tu dominio de frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -36,24 +36,9 @@ async function bootstrap() {
     .setTitle('API')
     .setDescription('The haptica API description')
     .setVersion('1.0')
-    // .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-
-  // const server = https.createServer(httpsOptions, app.getHttpAdapter().getInstance());
-
-  // // Iniciar el servidor en el puerto 443
-  
-  // server.listen(process.env.PORT || 3000, () => {
-  //   console.log('Servidor HTTPS escuchando en el puerto');
-  // });
-
-  // // Manejo de errores
-  // server.on('error', (error) => {
-  //   console.error('Error en el servidor:', error);
-  // });
-
 
   await app.listen(process.env.PORT || 3000);
 }
